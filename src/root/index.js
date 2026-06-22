1a99c19f72c75576bf9ff1764d02b8353b12d36a
= <=>
2026-06-22 17:15:42 -0500

    fixed controller for the customer lookup by company name

--- src/root/index.js
+++ src/root/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const rootController = require('./root.controller');
+const customerController = require('../modules/customer/customer.controller');
 const customerRoutes = require('../modules/customer/customer.routes');
 
 // GET /
@@ -8,5 +9,8 @@ router.get('/', rootController.getRoot);
 
 // Mount customer routes under /customer
 router.use('/customer', customerRoutes);
+
+// Expose company lookup at top-level: GET /company/:company_name
+router.get('/company/:company_name', customerController.getCustomersByCompany);
 
 module.exports = router;
