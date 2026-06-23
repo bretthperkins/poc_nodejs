b8dcb4733225aeb6cb1f6945d8c7c5ae8e021549
= <=>
2026-06-23 18:23:37 -0500

    implemented authentication

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const routes = require('./root/index');
+const { authenticateToken } = require('./middleware/auth');
 
 const app = express();
 
 app.use(express.json());
 
-// Register all routes
+// Apply authentication to ALL routes
+app.use(authenticateToken);
+
+// Register all routes (now protected)
 app.use('/', routes);
 
 module.exports = app;
